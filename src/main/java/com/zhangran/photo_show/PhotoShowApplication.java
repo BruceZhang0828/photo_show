3b825218bc8ec8679ddb447d019d89363ced95c2
zhangyi1015 <zycool0828@163.com>
2019-03-10 10:05:38 +0800

    Initial commit

---
+++ src/main/java/com/zhangran/photo_show/PhotoShowApplication.java
@@ -0,0 +1,13 @@
+package com.zhangran.photo_show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoShowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhangran/photo_show/PhotoShowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhangran.photo_show;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotoShowApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
