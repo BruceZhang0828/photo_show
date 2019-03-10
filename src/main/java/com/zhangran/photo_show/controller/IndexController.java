81afbeedfa74e1574f34b364e064b1289eb6dac2
zhy <zycool0828@163.com>
2019-03-10 17:26:26 +0800

    添加indexController

---
+++ src/main/java/com/zhangran/photo_show/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.zhangran.photo_show.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/index")
+public class IndexController  {
+
+    public String index(){
+        return "index";
+    }
+}
