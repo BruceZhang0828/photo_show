fb8c5d11ecccc1afe4a5aec747b6fe1c2539d58b
zhy <zycool0828@163.com>
2019-03-10 18:16:54 +0800

    添加indexController

--- src/main/java/com/zhangran/photo_show/controller/IndexController.java
+++ src/main/java/com/zhangran/photo_show/controller/IndexController.java
@@ -1,13 +1,16 @@
 package com.zhangran.photo_show.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(value = "/index")
+@Controller
 public class IndexController  {
 
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
     public String index(){
         return "index";
     }
